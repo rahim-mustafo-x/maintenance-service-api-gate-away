015464950b3201ff607f3db4ac8e229186aa2bbc
rahim-mustafo-x <rahim.mustafo.x@gmail.com>
2026-08-05 00:33:53 +0500

    maintenance-service-api-gateway is done

---
+++ src/main/java/org/safa/maintenanceserviceapigateaway/MaintenanceServiceApiGateAwayApplication.java
@@ -0,0 +1,13 @@
+package org.safa.maintenanceserviceapigateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceServiceApiGateAwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceServiceApiGateAwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/safa/maintenanceserviceapigateaway/MaintenanceServiceApiGateAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.safa.maintenanceserviceapigateaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceServiceApiGateAwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
